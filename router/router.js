524298804dd935101589f3b99cd310c9eab400e9
Pluer <ramonpvlima2@hotmail.com>
2023-11-26 17:24:30 -0300

    0.0.0.4

--- router/router.js
+++ router/router.js
@@ -4,12 +4,13 @@ let router = express.Router();
 let LoginController = require('../controller/loginController');
 let FuncionarioController = require('../controller/funcionarioController');
 
-router.get('/login', LoginController.search );
 router.post('/login', LoginController.access );
 router.post('/login/register', LoginController.register );
 router.post('/login/excluir', LoginController.delete );
 router.put('/login/editar', LoginController.update );
 
+/* router that needs permission to access */
+router.get('/login', LoginController.search );
 router.get('/funcionario', FuncionarioController.getAll );
 
 module.exports = router;
